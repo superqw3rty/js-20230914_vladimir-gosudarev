49e5b46e1eeb78652eed7e7b91dfc862cb615376
vgosudarev <vgosudarevv@gmail.com>
2023-10-09 17:53:41 +0400

    lesson-5/sortable

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -2,16 +2,26 @@
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
+ * @param value param for sorting object fields
+ * @param valueType param - the sorting type "string" or "number"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
+export function sortStrings(arr, param = 'asc', value = '', valueType = 'string') {
   if (!arr || !arr.length) {
     return null
   }
 
-  return param === 'asc' ? [...arr].sort((a, b) => sortFunction(a, b)) : [...arr].sort((a, b) => sortFunction(b, a));
+  if (!value) {
+    return param === 'asc' ? [...arr].sort((a, b) => sortFunction(a, b)) : [...arr].sort((a, b) => sortFunction(b, a));
+  }
+
+  return param === 'asc' ? [...arr].sort((a, b) => sortFunction(a, b, value, valueType)) : [...arr].sort((a, b) => sortFunction(b, a, value, valueType));
 }
 
-function sortFunction(word1, word2) {
-  return word1.localeCompare(word2, 'ru', {caseFirst: 'upper'})
+function sortFunction(a, b, value = '', type = '') {
+  if (type === 'number') {
+    return value ? a[value] - b[value] : a - b;
+  }
+
+  return value ? a[value].localeCompare(b[value], 'ru', {caseFirst: 'upper'}) : a.localeCompare(b, 'ru', {caseFirst: 'upper'});
 }
